d2b3cc2682a3e70dc31a78dc0d5318cc1dff0b1a
riskaardila <riskaardila35@gmail.com>
2021-11-20 08:11:09 +0700

    Penjualan Pakaian

---
+++ app/src/main/java/com/example/myjualangrup/MoveJualan1.kt
@@ -0,0 +1,12 @@
+package com.example.myjualangrup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveJualan1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_jualan1)
+        supportActionBar?.title = "Pleated skirt"
+    }
+}
---
+++ app/src/main/java/com/example/myjualangrup/MoveJualan2.kt
@@ -0,0 +1,12 @@
+package com.example.myjualangrup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveJualan2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_jualan2)
+        supportActionBar?.title = "Dress"
+    }
+}
